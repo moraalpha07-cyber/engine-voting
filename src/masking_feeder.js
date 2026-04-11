a5016d1fa51792998290540b1d1672b2196d35e7
dharanadamidu07-ops <dharanadamidu07@gmail.com>
2026-04-11 09:07:48 +0530

    Update Masking feeder: Use rolling 30s window for delta calculation

--- src/masking_feeder.js
+++ src/masking_feeder.js
@@ -36,370 +36,7 @@ if (!admin.apps.length) {
 const db = admin.database();
 
 const projects = [
-  "abinbevbr",
-  "abnz",
-  "altriaus",
-  "altriausdemo",
-  "aneuae",
-  "avidityuk",
-  "batru",
-  "bdftr",
-  "beiersdorfar",
-  "beiersdorfau",
-  "beiersdorfbe",
-  "beiersdorfbo",
-  "beiersdorfbr",
-  "beiersdorfchl",
-  "beiersdorfco",
-  "beiersdorfcz",
-  "beiersdorfde",
-  "beiersdorfec",
-  "beiersdorfeg",
-  "beiersdorffr",
-  "beiersdorfgr",
-  "beiersdorfgt",
-  "beiersdorfid",
-  "beiersdorfin",
-  "beiersdorfit",
-  "beiersdorfke",
-  "beiersdorfkz",
-  "beiersdorfmx",
-  "beiersdorfmy",
-  "beiersdorfng",
-  "beiersdorfnz",
-  "beiersdorfpe",
-  "beiersdorfph",
-  "beiersdorfpl",
-  "beiersdorfpt",
-  "beiersdorfpy",
-  "beiersdorfro",
-  "beiersdorfru",
-  "beiersdorfsa",
-  "beiersdorfse",
-  "beiersdorfsp",
-  "beiersdorfth",
-  "beiersdorftw",
-  "beiersdorfuae",
-  "beiersdorfuk",
-  "beiersdorfvn",
-  "beiersdorfza",
-  "bepensamx",
-  "bikr",
-  "bimboes",
-  "bimbomx",
-  "bimbous",
-  "biph",
-  "biseask",
-  "bivn",
-  "bluetritonusa",
-  "cbcdairyil",
-  "cbcil",
-  "ccaau",
-  "ccandinaar",
-  "ccanz",
-  "ccbr-prod",
-  "ccjp",
-  "ccjpvm",
-  "cckh",
-  "cckr",
-  "cclibertyus",
-  "ccphl",
-  "ccusdemo",
-  "ccza",
-  "colgatelatam",
-  "Comon",
-  "cpgdemo",
-  "danonear",
-  "danonejp",
-  "danoneuk",
-  "deltafoodsgr",
-  "diageoar",
-  "diageoau",
-  "diageobaltics",
-  "diageobenelux",
-  "diageobr",
-  "diageoca",
-  "diageoco",
-  "diageoes",
-  "diageoga",
-  "diageogh",
-  "diageogr",
-  "diageogtr",
-  "diageogtrassetpilot",
-  "diageoid",
-  "diageoie",
-  "diageoiedemo",
-  "diageoin",
-  "diageoit",
-  "diageojp",
-  "diageoke",
-  "diageokr",
-  "diageomx",
-  "diageong",
-  "diageopa",
-  "diageopebac",
-  "diageoph",
-  "diageopl",
-  "diageopt",
-  "diageoromania",
-  "diageosc",
-  "diageosg",
-  "diageostr",
-  "diageoth",
-  "diageotw",
-  "diageotz",
-  "diageoug",
-  "diageouk",
-  "diageous",
-  "diageovn",
-  "diageoza",
-  "dkshmy",
-  "dlcpt",
-  "dollargeneraldmxus",
-  "dreyerus",
-  "Edit Menu",
-  "Edit Normal",
-  "FactpharmaBE",
-  "fapharmabe",
-  "fapharmafr",
-  "fazerfi",
-  "femsaar",
-  "femsamx",
-  "ferreroid",
-  "ferreromy",
-  "ferreroph",
-  "ferrerosg",
-  "ferreroth",
-  "ferrerovn",
-  "fonterralk",
-  "frucorau",
-  "frucornz",
-  "gdsar",
-  "gmilac",
-  "gmkr",
-  "googlehk",
-  "googlekr",
-  "googlemx",
-  "googleusa",
-  "gpus",
-  "gskau",
-  "gskbg",
-  "gskch",
-  "gskcz",
-  "gskde",
-  "gskes",
-  "gskesph",
-  "gskfi",
-  "gskglobal",
-  "gskgr",
-  "gskhu",
-  "gskjp",
-  "gskkz",
-  "gsklt",
-  "gsknz",
-  "gskpl",
-  "gskro",
-  "gskruph",
-  "gsksg",
-  "gsksk",
-  "gsktw",
-  "gskua",
-  "gskuz",
-  "gskza",
-  "haleonaesa",
-  "haleonbr",
-  "HALEONHU",
-  "haleonil",
-  "haleonmy",
-  "haleonse",
-  "haleonvn",
-  "heinekenbr",
-  "heinekentw",
-  "heinzcr",
-  "henkeltr",
-  "hersheysusdemo",
-  "hphoodus",
-  "inbevci",
-  "inbevnl",
-  "intagejp2",
-  "jdetr",
-  "jdeza",
-  "jnjanz",
-  "jtiglobal",
-  "jtihr",
-  "jtimg",
-  "jtiro",
-  "jtisl",
-  "jtius",
-  "jtjp",
-  "kenvuelatam",
-  "kibonbr",
-  "kirinjp",
-  "labattplnoptca",
-  "LIGA",
-  "lightpilotdemo",
-  "lionnz",
-  "markanthonygroupus",
-  "marsbh",
-  "marsegy",
-  "marskw",
-  "marsmx",
-  "marsom",
-  "marspl",
-  "marsqa",
-  "marssa",
-  "marstr",
-  "marsuae",
-  "marsuk",
-  "mdlzdk",
-  "mdlzrusf",
-  "MENU",
-  "moethennessyar",
-  "moethennessyus",
-  "molsoncoorsuk",
-  "mondelezau",
-  "mondelezaz",
-  "mondelezca",
-  "mondelezde",
-  "mondelezdmius",
-  "mondelezeg",
-  "mondelezes",
-  "mondelezfi1",
-  "mondelezge",
-  "mondelezkaza",
-  "mondelezmy",
-  "mondelezno",
-  "mondelezprt",
-  "mondelezsa",
-  "mondelezse",
-  "mondelezsg",
-  "mondeleztr",
-  "mondelezukre",
-  "mondelezusps",
-  "mondelezusquality",
-  "mondelezuz",
-  "mondelezza",
-  "munchysmy",
-  "newellus",
-  "nrfbodycare",
-  "nrfleaftea",
-  "nrfsoftdrinks",
-  "odcbcil",
-  "odccbr-prod",
-  "oddiageoiedemo",
-  "odmondelezukre",
-  "odmondelezusps",
-  "odnrfbodycare",
-  "odnrfleaftea",
-  "odnrfsoftdrinks",
-  "odpngjp",
-  "odstraussdryil",
-  "odtempoil",
-  "odulpt",
-  "odunileveril",
-  "odunilevermx",
-  "odunileverus",
-  "penaflorar",
-  "pepsibe",
-  "pepsicoes",
-  "pepsicofr",
-  "pepsicopl",
-  "pepsicotr",
-  "pepsicouk",
-  "pepside",
-  "pepsidemoglobal",
-  "pepsigt",
-  "pernodin",
-  "pernodricardes",
-  "pernodus",
-  "pgbaltics2",
-  "pgcroatia",
-  "pgcz",
-  "pges",
-  "pgespharma",
-  "pghu",
-  "pgpl",
-  "pgpt",
-  "pgsk",
-  "pgua",
-  "pngbr",
-  "pngcn-prod",
-  "pnghk",
-  "pngjp",
-  "pngmx",
-  "pngvn",
-  "pngza2",
-  "pureaidemoamer",
-  "pureaidemoapac",
-  "pureaidemoemea",
-  "refriangoao",
-  "rinielsen2",
-  "risparkwinede",
-  "rjreynoldsus",
-  "sanofiae",
-  "sanofiar",
-  "sanofiat",
-  "sanofiau",
-  "sanofibe",
-  "sanofibr",
-  "sanofich",
-  "sanofico",
-  "sanoficz",
-  "sanofide",
-  "sanofiec",
-  "sanofieg",
-  "sanofies",
-  "sanofifr",
-  "sanofigr",
-  "sanofihu",
-  "sanofiit",
-  "sanofijp",
-  "sanofimx",
-  "sanofipl",
-  "sanofipt",
-  "sanofiro",
-  "sanofiru",
-  "sanofisa",
-  "sanofitr",
-  "sanofiua",
-  "schwartautkde",
-  "scjohnsonar",
-  "scjohnsonbr",
-  "sindicatedmx",
-  "sinoth",
-  "sksignals",
-  "solarbr",
-  "straussdryil",
-  "straussfritolayil",
-  "straussil",
-  "suntoryjp2",
-  "teamcorelatam",
-  "tempoil",
-  "tevade",
-  "tevapl",
-  "tevaru",
-  "tnuvailv2",
-  "traxtobaccous",
-  "tuborgro",
-  "ulbe",
-  "ulbr",
-  "ulde",
-  "ules",
-  "ulgr",
-  "ulit",
-  "ulnl",
-  "ulpt",
-  "ulse",
-  "uluk",
-  "unileverau",
-  "unileverco",
-  "unileveril",
-  "unileverken",
-  "unileverlk",
-  "unilevermx",
-  "unilevernz",
-  "unileverus",
-  "yalolatam"
+  "abinbevbr", "abnz", "altriaus", "altriausdemo", "aneuae", "avidityuk", "batru", "bdftr", "beiersdorfar", "beiersdorfau", "beiersdorfbe", "beiersdorfbo", "beiersdorfbr", "beiersdorfchl", "beiersdorfco", "beiersdorfcz", "beiersdorfde", "beiersdorfec", "beiersdorfeg", "beiersdorffr", "beiersdorfgr", "beiersdorfgt", "beiersdorfid", "beiersdorfin", "beiersdorfit", "beiersdorfke", "beiersdorfkz", "beiersdorfmx", "beiersdorfmy", "beiersdorfng", "beiersdorfnz", "beiersdorfpe", "beiersdorfph", "beiersdorfpl", "beiersdorfpt", "beiersdorfpy", "beiersdorfro", "beiersdorfru", "beiersdorfsa", "beiersdorfse", "beiersdorfsp", "beiersdorfth", "beiersdorftw", "beiersdorfuae", "beiersdorfuk", "beiersdorfvn", "beiersdorfza", "bepensamx", "bikr", "bimboes", "bimbomx", "bimbous", "biph", "biseask", "bivn", "bluetritonusa", "cbcdairyil", "cbcil", "ccaau", "ccandinaar", "ccanz", "ccbr-prod", "ccjp", "ccjpvm", "cckh", "cckr", "cclibertyus", "ccphl", "ccusdemo", "ccza", "colgatelatam", "Comon", "cpgdemo", "danonear", "danonejp", "danoneuk", "deltafoodsgr", "diageoar", "diageoau", "diageobaltics", "diageobenelux", "diageobr", "diageoca", "diageoco", "diageoes", "diageoga", "diageogh", "diageogr", "diageogtr", "diageogtrassetpilot", "diageoid", "diageoie", "diageoiedemo", "diageoin", "diageoit", "diageojp", "diageoke", "diageokr", "diageomx", "diageong", "diageopa", "diageopebac", "diageoph", "diageopl", "diageopt", "diageoromania", "diageosc", "diageosg", "diageostr", "diageoth", "diageotw", "diageotz", "diageoug", "diageouk", "diageous", "diageovn", "diageoza", "dkshmy", "dlcpt", "dollargeneraldmxus", "dreyerus", "Edit Menu", "Edit Normal", "FactpharmaBE", "fapharmabe", "fapharmafr", "fazerfi", "femsaar", "femsamx", "ferreroid", "ferreromy", "ferreroph", "ferrerosg", "ferreroth", "ferrerovn", "fonterralk", "frucorau", "frucornz", "gdsar", "gmilac", "gmkr", "googlehk", "googlekr", "googlemx", "googleusa", "gpus", "gskau", "gskbg", "gskch", "gskcz", "gskde", "gskes", "gskesph", "gskfi", "gskglobal", "gskgr", "gskhu", "gskjp", "gskkz", "gsklt", "gsknz", "gskpl", "gskro", "gskruph", "gsksg", "gsksk", "gsktw", "gskua", "gskuz", "gskza", "haleonaesa", "haleonbr", "HALEONHU", "haleonil", "haleonmy", "haleonse", "haleonvn", "heinekenbr", "heinekentw", "heinzcr", "henkeltr", "hersheysusdemo", "hphoodus", "inbevci", "inbevnl", "intagejp2", "jdetr", "jdeza", "jnjanz", "jtiglobal", "jtihr", "jtimg", "jtiro", "jtisl", "jtius", "jtjp", "kenvuelatam", "kibonbr", "kirinjp", "labattplnoptca", "LIGA", "lightpilotdemo", "lionnz", "markanthonygroupus", "marsbh", "marsegy", "marskw", "marsmx", "marsom", "marspl", "marsqa", "marssa", "marstr", "marsuae", "marsuk", "mdlzdk", "mdlzrusf", "MENU", "moethennessyar", "moethennessyus", "molsoncoorsuk", "mondelezau", "mondelezaz", "mondelezca", "mondelezde", "mondelezdmius", "mondelezeg", "mondelezes", "mondelezfi1", "mondelezge", "mondelezkaza", "mondelezmy", "mondelezno", "mondelezprt", "mondelezsa", "mondelezse", "mondelezsg", "mondeleztr", "mondelezukre", "mondelezusps", "mondelezusquality", "mondelezuz", "mondelezza", "munchysmy", "newellus", "nrfbodycare", "nrfleaftea", "nrfsoftdrinks", "odcbcil", "odccbr-prod", "oddiageoiedemo", "odmondelezukre", "odmondelezusps", "odnrfbodycare", "odnrfleaftea", "odnrfsoftdrinks", "odpngjp", "odstraussdryil", "odtempoil", "odulpt", "odunileveril", "odunilevermx", "odunileverus", "penaflorar", "pepsibe", "pepsicoes", "pepsicofr", "pepsicopl", "pepsicotr", "pepsicouk", "pepside", "pepsidemoglobal", "pepsigt", "pernodin", "pernodricardes", "pernodus", "pgbaltics2", "pgcroatia", "pgcz", "pges", "pgespharma", "pghu", "pgpl", "pgpt", "pgsk", "pgua", "pngbr", "pngcn-prod", "pnghk", "pngjp", "pngmx", "pngvn", "pngza2", "pureaidemoamer", "pureaidemoapac", "pureaidemoemea", "refriangoao", "rinielsen2", "risparkwinede", "rjreynoldsus", "sanofiae", "sanofiar", "sanofiat", "sanofiau", "sanofibe", "sanofibr", "sanofich", "sanofico", "sanoficz", "sanofide", "sanofiec", "sanofieg", "sanofies", "sanofifr", "sanofigr", "sanofihu", "sanofiit", "sanofijp", "sanofimx", "sanofipl", "sanofipt", "sanofiro", "sanofiru", "sanofisa", "sanofitr", "sanofiua", "schwartautkde", "scjohnsonar", "scjohnsonbr", "sindicatedmx", "sinoth", "sksignals", "solarbr", "straussdryil", "straussfritolayil", "straussil", "suntoryjp2", "teamcorelatam", "tempoil", "tevade", "tevapl", "tevaru", "tnuvailv2", "traxtobaccous", "tuborgro", "ulbe", "ulbr", "ulde", "ules", "ulgr", "ulit", "ulnl", "ulpt", "ulse", "uluk", "unileverau", "unileverco", "unileveril", "unileverken", "unileverlk", "unilevermx", "unilevernz", "unileverus", "yalolatam"
 ];
 
 const metrics = [
@@ -419,12 +56,8 @@ function fetchWithTimeout(url, options, timeoutMs = 8000) {
 async function fetchProject(project) {
   const payloadParts = [];
   metrics.forEach(m => {
-    payloadParts.push(
-      `target=alias(prod.gauges.selector.queue.${m.path}.${project}.total,'${m.name} - Total')`
-    );
-    payloadParts.push(
-      `target=alias(aliasByNode(prod.gauges.selector.queue.${m.path}.${project}.oldestTask,4),'${m.name} - Oldest Task')`
-    );
+    payloadParts.push(`target=alias(prod.gauges.selector.queue.${m.path}.${project}.total,'${m.name} - Total')`);
+    payloadParts.push(`target=alias(aliasByNode(prod.gauges.selector.queue.${m.path}.${project}.oldestTask,4),'${m.name} - Oldest Task')`);
   });
   const payload = payloadParts.join("&") + "&from=-1h&until=now&format=json";
 
@@ -437,40 +70,21 @@ async function fetchProject(project) {
     body: payload
   }, 8000);
 
-  if (!response.ok) {
-    const errorText = await response.text();
-    console.error(`❌ Error ${project}: ${response.status} - ${errorText.substring(0, 100)}`);
-    return null;
-  }
+  if (!response.ok) return null;
 
   const json = await response.json();
-
   const groupedData = {};
   json.forEach(series => {
-    const lastValidPoint = series.datapoints
-      .filter(dp => dp[0] !== null)
-      .pop();
+    const lastValidPoint = series.datapoints.filter(dp => dp[0] !== null).pop();
     if (lastValidPoint) {
       const timestamp = lastValidPoint[1] * 1000;
       const value = lastValidPoint[0];
       const isOldestTask = series.target.includes("Oldest Task");
-      const metricName = series.target
-        .replace(" - Total", "")
-        .replace(" - Oldest Task", "");
+      const metricName = series.target.replace(" - Total", "").replace(" - Oldest Task", "");
 
-      if (!groupedData[metricName]) {
-        groupedData[metricName] = {
-          lastUpdated: timestamp,
-          total: null,
-          oldestTask: null
-        };
-      }
-      if (isOldestTask) {
-        groupedData[metricName].oldestTask = value;
-      } else {
-        groupedData[metricName].total = value;
-        groupedData[metricName].lastUpdated = timestamp;
-      }
+      if (!groupedData[metricName]) groupedData[metricName] = { lastUpdated: timestamp, total: null, oldestTask: null };
+      if (isOldestTask) groupedData[metricName].oldestTask = value;
+      else { groupedData[metricName].total = value; groupedData[metricName].lastUpdated = timestamp; }
     }
   });
 
@@ -479,102 +93,74 @@ async function fetchProject(project) {
 
 // 🔹 Main loop
 async function main() {
-  console.log("🚀 Starting Masking fetch cycle...");
+  console.log("🚀 Starting Masking fetch cycle (30s Rolling Delta)...");
 
   const RUN_DURATION_MS = 55 * 1000;
-  const INTERVAL_MS     = 5 * 1000;
-  const MIN_WAIT_MS     = 1000;
+  const INTERVAL_MS     = 5000;
+  const HISTORY_SIZE    = 6; // 6 * 5s = 30s window
 
   const startTime = Date.now();
   let cycleCount = 0;
+  let history = []; // Rolling buffer of snapshots for 30s diff
 
-  console.log("📥 Fetching baseline data from Firebase (masking/grafana/queue_metrics)...");
-  let baselineData = {};
+  // Initialize history with current Firebase state to start with some diff data
   try {
-    const snapshot = await db.ref("masking/grafana/queue_metrics").once("value");
-    if (snapshot.exists()) {
-      baselineData = snapshot.val();
-      console.log("✅ Baseline data loaded.");
+    const snap = await db.ref("masking/grafana/queue_metrics").once("value");
+    if (snap.exists()) {
+      const initial = snap.val();
+      for (let i = 0; i < HISTORY_SIZE; i++) history.push(initial);
     }
-  } catch (err) {
-    console.warn("⚠️ Could not fetch baseline data:", err.message);
-  }
+  } catch (e) {}
 
-  const hardKillTimer = setTimeout(() => {
-    console.log("⏱️ Hard kill timer triggered. Force exiting.");
-    process.exit(0);
-  }, 58 * 1000);
+  const hardKillTimer = setTimeout(() => process.exit(0), 58 * 1000);
 
   while (Date.now() - startTime < RUN_DURATION_MS) {
     const cycleStart = Date.now();
     cycleCount++;
-    console.log(`🔄 Masking Cycle #${cycleCount} started at ${new Date().toISOString()}`);
+    console.log(`🔄 Cycle #${cycleCount} [30s rolling diff]`);
 
     try {
-      const results = await Promise.all(
-        projects.map(async project => {
-          try {
-            const data = await fetchProject(project);
-            return { project, data };
-          } catch (err) {
-            console.error(`❌ Project ${project} failed: ${err.message}`);
-            return { project, data: null };
-          }
-        })
-      );
+      const results = await Promise.all(projects.map(async p => ({ project: p, data: await fetchProject(p) })));
 
-      const allData = {};
-      results.forEach(({ project, data }) => {
-        if (!data) return;
+      const currentSnapshot = {};
+      results.forEach(({ project, data }) => { if (data) currentSnapshot[project] = data; });
 
-        const processedData = {};
-        Object.keys(data).forEach(metricName => {
-          const current = data[metricName];
-          const baseline = baselineData[project] ? baselineData[project][metricName] : null;
-          
-          let delta = 0;
-          if (baseline && baseline.total !== null && current.total !== null) {
-            delta = current.total - baseline.total;
-          }
+      // Calculate deltas against 30s ago (oldest in buffer)
+      const baseline = history.length > 0 ? history[0] : currentSnapshot;
+      const allDataWithDelta = {};
 
-          processedData[metricName] = {
-            ...current,
-            minuteDelta: delta
-          };
+      Object.keys(currentSnapshot).forEach(project => {
+        const currentM = currentSnapshot[project];
+        const baselineM = baseline[project] || {};
+        const processed = {};
+
+        Object.keys(currentM).forEach(mName => {
+            const cur = currentM[mName];
+            const base = baselineM[mName] || { total: cur.total };
+            processed[mName] = { ...cur, minuteDelta: cur.total - base.total };
         });
-
-        allData[project] = processedData;
+        allDataWithDelta[project] = processed;
       });
 
-      await Promise.race([
-        db.ref("masking/grafana/queue_metrics").set({
-          ...allData,
-          _lastUpdated: Date.now()
-        }),
-        new Promise((_, reject) =>
-          setTimeout(() => reject(new Error("Firebase write timeout")), 4000)
-        )
-      ]);
+      // Update history buffer
+      history.push(currentSnapshot);
+      if (history.length > HISTORY_SIZE) history.shift();
 
-      console.log(`✅ Firebase (Masking) updated at ${new Date().toISOString()}`);
+      // Push to Firebase
+      await db.ref("masking/grafana/queue_metrics").set({ ...allDataWithDelta, _lastUpdated: Date.now() });
+      console.log(`✅ Updated at ${new Date().toISOString()}`);
+
     } catch (e) {
       console.error("❌ Cycle error:", e.message);
     }
 
-    const cycleEnd = Date.now();
-    const nextCycleTarget = cycleStart + INTERVAL_MS;
-    const waitTime = Math.max(MIN_WAIT_MS, nextCycleTarget - cycleEnd);
-    await new Promise(resolve => setTimeout(resolve, waitTime));
+    const waitTime = Math.max(1000, (cycleStart + INTERVAL_MS) - Date.now());
+    await new Promise(r => setTimeout(r, waitTime));
   }
 
   clearTimeout(hardKillTimer);
-  console.log(`✅ Done. ${cycleCount} cycles completed. Exiting.`);
   await admin.app().delete();
   process.exit(0);
 }
 
-main().catch(err => {
-  console.error("💥 Fatal error in main loop:");
-  console.error(err);
-  process.exit(1);
-});
+main().catch(err => { console.error(err); process.exit(1); });
